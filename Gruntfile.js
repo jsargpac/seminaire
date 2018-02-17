a74b06ab7130164682664178ee835025260baa59
jsargpac <juliencapgras@gmail.com>
2018-02-17 21:33:13 +0100

    packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
         app: {
             source: 'app',
             dist: 'dist',
-            baseurl: 'zeppelin-grunt',
-            git_repo: 'git@github.com:gdg-x/zeppelin-grunt.git',
+            baseurl: 'seminaire',
+            git_repo: 'git@github.com:jsargpac/seminaire.git',
             branch: 'gh-pages'
         },
         watch: {
